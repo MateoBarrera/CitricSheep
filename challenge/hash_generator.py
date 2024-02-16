501e5298dfb8909097dbf6a348a951b9450f2583
MateoBarrera <materile1997@gmail.com>
2024-02-16 00:09:15 -0500

    feat: all done

--- challenge/hash_generator.py
+++ challenge/hash_generator.py
@@ -11,30 +11,53 @@
   - Convert this hash to a hexadecimal string.
 """
 
-from hashlib import sha256
+import hashlib
 
 
-def _string_tolist(text):
+def string_to_list_ascii(text):
+    """
+    The string_to_list_ascii function takes a string as input and returns a list of the ASCII values for each character in the string.
+
+    :param text: Get the string that will be converted to a list of ascii values
+    :return: The list of the ascii values of each character in a string
+    :doc-author: Trelent
+    """
     list_text = list(text)
     list_text_ascii = [ord(x) for x in list_text]
     return list_text_ascii
 
 
-def _hast256(list_ascii):
+def hash_generator(list_ascii):
+    """
+    The hash_generator function takes a list of ASCII values as input and returns the SHA256 hash of the sum
+    of each ASCII value multiplied by the length of the list. This is done to ensure that if two lists have different
+    lengths, they will not produce identical hashes.
+
+    :param list_ascii: Pass the list of ascii values to the function
+    :return: A hash of the sum of the ascii values multiplied by their position in the string
+    :doc-author: Trelent
+    """
     n_characters = len(list_ascii)
     list_multiplied = [x * n_characters for x in list_ascii]
-    sum_to_hash = sum(list_multiplied)
-
-    hast_result = sha256(str(sum_to_hash))
-
-    return hast_result
+    sum_to_hash = str(sum(list_multiplied))
+    h = hashlib.sha256(str.encode(sum_to_hash))
+    return h.hexdigest()
 
 
-def create_password(text):
-    list_ascii = _string_tolist(text)
-    hash_value = _hast256(list_ascii)
+def encrypt_password(text):
+    """
+    The encrypt_password function takes a string as input and returns the hash value of that string.
+    The function first converts the string to a list of ASCII values, then uses those values to generate
+    a hash value using the SHA256 algorithm.
+
+    :param text: Pass the string that is going to be encrypted
+    :return: The hash value of the text
+    :doc-author: Trelent
+    """
+    list_ascii = string_to_list_ascii(text)
+    hash_value = hash_generator(list_ascii)
     return hash_value
 
 
 if __name__ == "__main__":
-    print(create_password("CitricSheep"))
+    print(encrypt_password("CitricSheep"))
